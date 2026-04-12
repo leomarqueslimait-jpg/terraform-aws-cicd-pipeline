29704318d8b7fe4b725b2c42325ab0c0e091571b
leomarqueslimait-jpg <leomarqueslimait@gmail.com>
2026-04-12 15:14:49 -0500

    add3 sync step and website files

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "static_site" {
 
 resource "aws_s3_bucket_public_access_block" "static_site" {
   bucket = aws_s3_bucket.static_site.id
-  
+
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
